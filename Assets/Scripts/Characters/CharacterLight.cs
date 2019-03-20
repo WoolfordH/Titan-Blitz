18f097e994e4eb96a6fab40892c16d59e80de73b
THE-BEAST\Ethan <EthanGill@connect.glos.ac.uk>
2019-03-20 18:07:21 +0000

    Testing Character revision

---
+++ Assets/Scripts/Characters/CharacterLight.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLight : Character {
+
+	// Use this for initialization
+	protected override void Start () {
+
+		base.Start ();
+		handler = GetComponent<CharacterHandler> ();
+
+		primaryDmg = new DAMAGE (2);
+
+		primaryRange = 100f;
+
+		abilities [0] = new AbilityGrapple ();
+		abilities [1] = new AbilityXRay();
+
+	}
+
+
+	public override void PrimaryAttack()
+	{
+
+		float dmgMod = 1f;
+
+		if (handler.powerups.Exists (x => x.powerupType.ToString () == "Damage"))
+		{
+			dmgMod = handler.powerups.Find (x => x.powerupType.ToString () == "Damage").multiplier;
+		}
+			
+
+		//Gun
+		if (primaryTimer <= 0f)
+		{
+			Fire ();
+			primaryTimer = primaryDelay;
+		}
+	}
+
+	void Fire()
+	{
+		Projectile proj = Instantiate(primaryProj, handler.muzzlePos.position, handler.muzzlePos.rotation).GetComponent<Projectile>();
+		proj.owners.Add(this.transform);
+		proj.dmg = primaryDmg;
+	}
+
+}
